ddf90dd45df13ce0e9bfd2ec5676b5b230021cd4
Gabryelle SS <gabryelle-gaby@hotmail.com>
2019-02-17 11:57:42 -0300

    Estruturando o jogo

---
+++ scripts.js
@@ -0,0 +1,28 @@
+// 1 - O usuario deve escolher se quer X ou O;
+// 2 - Se o contador atigir 9 será velha, fim de jogo;
+// 3 - Jogador um seleciona um quadrado. 
+// 4 - Se o contador for maior ou igual a cinco, entrar no laco;
+// 5 - 
+
+// let quadradoUm = document.querySelector('quad-um');
+// let quadradoDois = document.querySelector('quad-dois');
+// let quadradoTres = document.querySelector('quad-tres');
+// let quadradoQuatro = document.querySelector('quad-quatro');
+// let quadradoCinco = document.querySelector('quad-cinco');
+// let quadradoSeis = document.querySelector('quad-seis');
+// let quadradoSete = document.querySelector('quad-sete');
+// let quadradoOito = document.querySelector('quad-oito');
+// let quadradoNove = document.querySelector('quad-nove');
+
+let i = 0;
+
+let jogarUm;
+let jogarDois;
+//Seleciona O = 1 ou X = 2
+jogadorUm = 1
+jogadorDois = 2
+
+quadroDois = 2
+
+if
+´[dradoQuatro = jogadorUm
